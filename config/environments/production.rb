6d2c784015cb8b1606a647b1608735c4a20a15da
mikethreels <michaelthreels@gmail.com>
2021-09-04 01:28:50 +0200

    fixing issue with heroku/devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { :host => 'https://reservate-api.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    :enable_starttls_auto => true,
+    :authentication       => :plain,
+    :domain               => "yourdomain.com"
+  }
 end
