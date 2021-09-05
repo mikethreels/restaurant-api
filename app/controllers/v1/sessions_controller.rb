b8971c07692a1e7b5dc6763a9cee9e5a2a1a9ed3
mikethreels <michaelthreels@gmail.com>
2021-09-05 21:42:33 +0200

    removed commented code

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -9,27 +9,10 @@ class V1::SessionsController < ApplicationController
                            include: { reservations: { include: { store: { only: %i[id
                                                                                    name
                                                                                    location] } }, only: :date } })
-        # reservations: userReservations.as_json(include: { stores: { only: :name} })
       }
     else
       json_response(:unauthorized)
     end
-    # user = User
-    #         .find_by(email: params["email"])
-    # if user&.valid_password?(params[:password])
-    #   session[:user_id] = user.id
-    #   render json: {
-    #     status: :created,
-    #     logged_in: true,
-    #     user: user.as_json(only: %i[id email authentication_token],
-    #                        include: { reservations: { include: { store: { only: %i[id
-    #                                                                                name
-    #                                                                                location] } }, only: :date } })
-    #     # reservations: userReservations.as_json(include: { stores: { only: :name} })
-    #   }
-    # else
-    #   head(:unauthorized)
-    # end
   end
 
   def destroy; end
